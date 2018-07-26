916993d4a2625f96be43daa6642b742bab3e3217
vishal-saxena <vishalsax@gmail.com>
2018-07-26 14:38:13 +0100

    Add files via upload

--- python/explore/swaptionPlotting/OTMLinearRegression.py
+++ python/explore/swaptionPlotting/OTMLinearRegression.py
@@ -16,6 +16,9 @@ blue_dots = mpatches.Patch(color='blue', label='cleansed')
 grey_line = mpatches.Patch(color='grey', label='murex - smoothed int/ext polated -990:+990:10bps')
 orange_line = mpatches.Patch(color='orange', label='OLS cubic')
 green_dots = mpatches.Patch(color='green', label='Missing or < reqd. num contribs on pricing grid')
+pink_dots = mpatches.Patch(color='pink', label='delta +(cleansed) x(flattened vol)')
+cyan_dots = mpatches.Patch(color='cyan', label='vega +(cleansed) x(monotonic breach fixed)')
+
 
 pp = PdfPages('OTM-OLS_Cubic-CNN_NomuraJun12.pdf')
 
@@ -24,8 +27,11 @@ def OTMLinearRegression3(expiry, maturity, cleansed, smoothed, murex):
     print("Plotting %s-%s" % (expiry, maturity))
 
     # OTM
-    plt.title("%s-%s" % (expiry, maturity))
-    plt.legend(handles=[blue_dots, green_dots, red_dots, grey_line, orange_line])
+    plt.title("Expiry-Maturity(Days): %s-%s" % (expiry, maturity))
+    plt.xlabel("RelativeStrike (Bps)")
+    plt.ylabel("Vol (Bps)")
+
+    plt.legend(handles=[green_dots, blue_dots, pink_dots,cyan_dots,red_dots, grey_line, orange_line])
 
     otm = cleansed.loc[(cleansed['Expiry'] == expiry) & (cleansed['Maturity'] == maturity)]
     smile = otm.iloc[:, [2, 3]]
@@ -60,9 +66,14 @@ def OTMLinearRegression3(expiry, maturity, cleansed, smoothed, murex):
     # OTM smoothed
 
     otmSmoothed = smoothed.loc[(smoothed['Expiry'] == expiry) & (smoothed['Maturity'] == maturity)]
-    smileSmoothed = otmSmoothed.iloc[:, [2, 3]]
+    smileSmoothed = otmSmoothed.iloc[:, [2,3,4,5,6,7]]
     xSmoothed = smileSmoothed["Strike"]
     ySmoothed = smileSmoothed["Vol"]
+    deltas = smileSmoothed["Deltas"]
+    subsVolsDelta = smileSmoothed["SubsVolDeltas"]
+    vegas = smileSmoothed["Vegas"]
+    subsVolsVegas = smileSmoothed["SubsVolVegas"]
+
     plt.scatter(xSmoothed, ySmoothed, facecolors='none', edgecolors='Red')
 
     # OTM murex
@@ -73,8 +84,23 @@ def OTMLinearRegression3(expiry, maturity, cleansed, smoothed, murex):
     yMurex = smileMurex["Vol"]
     plt.scatter(xMurex, yMurex, c="Black", alpha=.1)
 
-    pp.savefig()
+    #OTM greeks used in smoothing
+    deltaAxis = plt.twinx()
+    deltaAxis.spines['left'].set_position(('outward', 12))
+    deltaAxis.set_ylabel("Delta",color ="Pink")
 
+    deltaAxis.scatter(xSmoothed, deltas, c="Pink", marker='+')
+    deltaAxis.scatter(xSmoothed, subsVolsDelta, c="Pink", marker='x')
+
+    vegaAxis = plt.twinx()
+    vegaAxis.set_ylabel("Vega", color="Cyan")
+    vegaAxis.spines['right'].set_position(('outward', 12))
+
+    vegaAxis.scatter(xSmoothed, vegas, c="Cyan", marker ='+')
+    vegaAxis.scatter(xSmoothed, subsVolsVegas, c="Cyan", marker ='x')
+
+
+    pp.savefig()
     plt.close()
 
     # yPFitAroundATM = yPfit[80:120]
@@ -90,13 +116,9 @@ expiries = np.array(
      10950])
 maturities = np.array([365, 730, 1095, 1460, 1825, 2190, 2555, 2920, 3285, 3650, 4380, 5475, 7300, 9125, 10950])
 
-# cleansedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/RoadToRuinMar15/USD_Cleansed_VolCube.csv"
-# smoothedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/RoadToRuinMar15/USD_ShapeChecked_VolCube.csv"
-# murexVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/RoadToRuinMar15/MurexCubeVolCube.csv"
-
-cleansedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined/USD_Cleansed_PricingGrid_VolCube.csv"
-smoothedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined/zeroDeltaThreshold/USD_ShapeChecked_VolCube.csv"
-murexVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined/zeroDeltaThreshold/MurexCubeVolCube.csv"
+cleansedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined_QC1592/USD_Cleansed_PricingGrid_VolCube.csv"
+smoothedVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined_QC1592/USD_ShapeChecked_VolCube.csv"
+murexVolCubeFilePath = "R:/Vishal.Saxena/SMART/Swaptions/alternative/CNNNomuraCombined_QC1592/MurexCubeVolCube.csv"
 
 # data
 cleansed = pd.read_csv(
@@ -112,16 +134,23 @@ murex = pd.read_csv(
 
 atmC = cleansed[cleansed['Strike'] == 0]
 ax = plt.axes(projection='3d')
-ax.set_title('ATM Cleansed X|Y|Z Expiry|Maturity|Vol');
-ax.plot_trisurf(atmC["Expiry"], atmC["Maturity"], atmC["Vol"], linewidth=.1, cmap=cm.Blues)
-pp.savefig()
-plt.close()
+ax.set_xlabel("Expiry (Days)")
+ax.set_ylabel("Maturity (Days)")
+ax.set_zlabel("Vol (bps)")
+# ax.set_title('ATM Cleansed X|Y|Z Expiry|Maturity|Vol');
+# ax.plot_trisurf(atmC["Expiry"], atmC["Maturity"], atmC["Vol"], linewidth=.1, cmap=cm.Blues)
+cleansedScatter = ax.scatter3D(atmC["Expiry"], atmC["Maturity"], atmC["Vol"],'blue')
+# pp.savefig()
+# plt.close()
 
 # ATM Smoothed
 atmS = smoothed[smoothed['Strike'] == 0]
-axS = plt.axes(projection='3d')
-axS.plot_trisurf(atmS["Expiry"], atmS["Maturity"], atmS["Vol"], linewidth=.1, cmap=cm.Reds)
-axS.set_title('ATM Smoothed X|Y|Z Expiry|Maturity|Vol');
+# axS = plt.axes(projection='3d')
+# axS.plot_trisurf(atmS["Expiry"], atmS["Maturity"], atmS["Vol"], linewidth=.1, cmap=cm.Reds)
+# axS.set_title('ATM Smoothed X|Y|Z Expiry|Maturity|Vol');
+smoothScatter = ax.scatter3D(atmS["Expiry"], atmS["Maturity"], atmS["Vol"],facecolors='none', edgecolors='r')
+
+ax.legend((cleansedScatter,smoothScatter),('Cleansed','Smoothed'))
 pp.savefig()
 plt.close()
 
